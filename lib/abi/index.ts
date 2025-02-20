d0c8d3694aee3d3c8802e33883607603336c29dc
alperenbekci <alperenbekci@hotmail.com>
2025-02-20 15:34:19 +0300

    types: create abi types

---
+++ lib/abi/index.ts
@@ -0,0 +1,9 @@
+import AccountFactory from './AccountFactory.json';
+import EntryPoint from './EntryPoint.json';
+import SmartAccount from './SmartAccount.json';
+import MinimalPaymaster from './MinimalPaymaster.json';
+
+export { default as AccountFactoryABI } from './AccountFactory.json';
+export { default as EntryPointABI } from './EntryPoint.json';
+export { default as SmartAccountABI } from './SmartAccount.json';
+export { default as MinimalPaymasterABI } from './MinimalPaymaster.json'; 
